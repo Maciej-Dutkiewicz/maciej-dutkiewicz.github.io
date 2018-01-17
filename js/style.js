f9d8b3783a27a6a87373a6ee6895a4162ad44c31
Maciek <m.p.dutkiewicz@gmail.com>
2018-01-17 18:26:41 +0100

    P_2.0

---
+++ js/style.js
@@ -0,0 +1,8 @@
+
+function openNav(pageOpen) {
+    document.getElementById(pageOpen).style.display = "block";
+}
+
+function closeNav(pageOpen) {
+    document.getElementById(pageOpen).style.display = "none";
+}
